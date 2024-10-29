a6d85f3d9238911b085a6528878a834e7f2a4367
Magelon <lishchenko.ivan@gmail.com>
2024-10-29 11:27:17 +0300

    Added tests for return and if optimizer

---
+++ test_code/ifReturn.d
@@ -0,0 +1,7 @@
+var tribonacci := func(n) is
+	print "Before return"
+	return 0
+	print "After return"
+end
+
+var x := tribonacci(10)
---
+++ test_code/ifTrue.d
@@ -0,0 +1,15 @@
+if true then
+	print "True it is"
+else
+	print "False it must be"
+end
+
+if false then
+	print "True it is"
+else
+	print "False it must be"
+end
+
+if false then
+	print "True it is"
+end
